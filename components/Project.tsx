e34309cdd29575c0d28a773f6ffa5bc4a5558f59
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2024-11-06 18:04:36 +0200

    Final touches

--- components/Project.tsx
+++ components/Project.tsx
@@ -33,8 +33,8 @@ export default function Project({
       }}
       className=" group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="group-even:pl-8 dark:text-white rounded-lg bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative h-[20rem] hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-10 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <section className="sm:group-even:pl-8 dark:text-white rounded-lg bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative h-[20rem] hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-10 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -55,7 +55,7 @@ export default function Project({
           src={imageURL}
           alt="Project I worked on"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 transition group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 transition group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
         />
       </section>
     </motion.div>
